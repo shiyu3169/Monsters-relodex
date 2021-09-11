084f99ed4f6eab296d9f6db1b0878fe60092d1be
Shiyu Wang <shiyu3169@gmail.com>
2021-09-11 16:19:51 -0700

    start of the example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      monsters: [],
+    }
+  }
+
+  onClick = () => {
+    this.setState({ string: this.state.string + 1 })
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users")
+      const data = await res.json()
+      this.setState({ monsters: data })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
