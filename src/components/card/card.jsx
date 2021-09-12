02ff0f53fd2bac3f911c60551be769eb0d938de6
Shiyu Wang <shiyu3169@gmail.com>
2021-09-11 17:05:24 -0700

    more about card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,8 +4,12 @@ import "./card.css"
 export const Card = ({ monster }) => {
   return (
     <div className="card-container">
-      <img src={`https://robohash.org/${monster.id}?set=set2`} alt="" />
-      <h1>{monster.name}</h1>
+      <img
+        src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+        alt=""
+      />
+      <h2>{monster.name}</h2>
+      <p>{monster.email}</p>
     </div>
   )
 }
